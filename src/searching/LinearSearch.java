483f999086d1c01872132e1df558fe29964d42bb
pmc0707 <pranshugoodboy@gmail.com>
2024-03-12 15:23:23 +0530

    Searching Algorithm

---
+++ src/searching/LinearSearch.java
@@ -0,0 +1,23 @@
+package searching;
+
+public class LinearSearch {
+    public static int linearsearch(int[] numbers, int key) {
+        for (int index = 0; index < numbers.length; index++) {
+            if (numbers[index] == key) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+        int index1 = linearsearch(numbers, key);
+        if (index1 == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("key is at index : " + index1);
+        }
+    }
+}
