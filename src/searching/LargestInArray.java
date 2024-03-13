3b87e26f6386afacb870b5d6f20a0943eef96804
pmc0707 <pranshugoodboy@gmail.com>
2024-03-13 07:41:42 +0530

    find the largest nummber in the given array

---
+++ src/searching/LargestInArray.java
@@ -0,0 +1,17 @@
+package searching;
+
+public class LargestInArray {
+    public static  int getLargest(int[] numbers){
+        int largest = Integer.MIN_VALUE;
+        for(int index= 0;index<numbers.length;index++){
+            if(largest<numbers[index]){
+                largest = numbers[index];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,8,4};
+        System.out.println("Largest value is : " + getLargest(numbers));
+    }
+}
